14539036267f9c8d5459a32afaecf56f44531bee
Ken Mugrage <kmugrage@thoughtworks.com>
2014-06-10 18:13:08 -0700

    updated example

--- example.php
+++ example.php
@@ -1,9 +1,20 @@
 <?php
 
 require_once("hello_library.php");
-
-$helloThere = new HelloWorld();
-
-$helloThere->sayHi();
+require_once("math_library.php");
 
 ?>
+
+<p>Hello There</p>
+<?php
+$helloThere = new HelloWorld();
+$helloThere->sayHi();
+?>
+
+<p>Basic Math</p>
+<?php
+$doMath = new BasicMath();
+
+print "2 + 2 = " . $doMath->addStuff(2, 2)
+
+?> 
