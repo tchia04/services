d6ff48b15cd4990a20c2d9c0ffbd2fffb6950b86
Workshop User <none@example.com>
2014-06-13 10:23:01 -0700

    running with one directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@localhost:workshop/services'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/var/www/html/prod/services'
+set :deploy_to, '/var/www/production/services'
 
 # Default value for :scm is :git
 # set :scm, :git
